6ebe82ebbbaf47753e2f376adf7eb1949c9a0870
afidhehe <miftahullafid@gmail.com>
2023-09-27 21:44:15 +0700

    this is test doctype

---
+++ gesitdev/gesiterp_dev_0/doctype/sample_app_1/sample_app_1.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, gesitERP.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sample App 1', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ gesitdev/gesiterp_dev_0/doctype/sample_app_1/sample_app_1.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, gesitERP.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class SampleApp1(Document):
+	pass
